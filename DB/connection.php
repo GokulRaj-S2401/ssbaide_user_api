90378fa67cfbe8961f16a8a6c9323a439172cc08
kalaiarasan27 <kalai73579@gmail.com>
2024-04-12 19:26:49 +0530

    validate_user  api updated

--- DB/connection.php
+++ DB/connection.php
@@ -2,14 +2,8 @@
 
 $servername = "localhost"; 
 $username = "root"; 
-<<<<<<< Updated upstream
 $password = "ssbaide220";
 $database = "ssbaide"; 
-=======
-$password = "ssbaide220"; 
-$datbase = "ssbaide";
-$con = new mysqli($servername, $username, $password,$datbase);
->>>>>>> Stashed changes
 
 $con = new mysqli($servername, $username, $password, $database);
 
